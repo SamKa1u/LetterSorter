3ac9fa863e10f4b4922d68f7b073a0a405da0437
Samuel Kalu <samkalu@ttu.edu>
2025-10-14 15:02:13 -0500

    update client to determine region based on location response

--- Pi_client/client.py
+++ Pi_client/client.py
@@ -3,6 +3,9 @@ import cv2
 from picamera2 import Picamera2
 import time
 
+# localization
+from uszipcode import SearchEngine
+
 # fastapi URL config
 base_url = "http://100.95.80.63:8080"
 endpoints = ["/","/receive_img"]
@@ -21,37 +24,55 @@ cam.configure(config)
 cam.start()
 time.sleep(1)
 
-while True:
-    # preview with openCV
-    frame =  cam.capture_array()
-    if frame is None:
-        print('[Picam] frame skipped')
-        continue
-    cv2.imshow("frame", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+def get_us_region(zipcode):
+    """
+    Takes a U.S. zip code and returns the Census region.
+    """
+    search = SearchEngine()
+    zip_info = search.by_zipcode(zipcode)
+    if zip_info:
+        return {
+            "region": zip_info.major_city_state.region,
+        }
+    return None
 
-    # capture and send image of letter if c key is pressed
-    k = cv2.waitKey(1)
-    if k  == ord('c'):
-        # save letter image
-        cam.capture_file("letter_image.png")
-       
-        # send letter image to LetterSorter app
-        try:
-            files = {'file': open('letter_image.png', 'rb')}
-        except Exception as e:
-            print(f"letter image file could not be opened: {e}")  # exit program if letter image not saved
-            break
-       
-        location = rq.post(base_url + endpoints[1], files=files, params=URLparams[0])
-        status = location.status_code
-        if status != 200:
-            print(f"[response error] status code: {status},response: {location.content}")
+def main():
+    while True:
+        # preview with openCV
+        frame =  cam.capture_array()
+        if frame is None:
+            print('[Picam] frame skipped')
             continue
-        else:
-            print(location.content)
-       
-    # exit program on 'esc' key pressed
-    elif k == 27:
-        break
+        cv2.imshow("frame", cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-cv2.destroyAllWindows()
+        # capture and send image of letter if c key is pressed
+        k = cv2.waitKey(1)
+        if k  == ord('c'):
+            # save letter image
+            cam.capture_file("letter_image.png")
+
+            # send letter image to LetterSorter app
+            try:
+                files = {'file': open('letter_image.png', 'rb')}
+            except Exception as e:
+                print(f"letter image file could not be opened: {e}")  # exit program if letter image not saved
+                break
+
+            location = rq.post(base_url + endpoints[1], files=files, params=URLparams[0])
+            status = location.status_code
+            if status != 200:
+                print(f"[response error] status code: {status},response: {location.content}")
+                continue
+            else:
+                print(location.content)
+                zipcode = location.content[-1:-5]
+                region = get_us_region(zipcode)
+                print(region)
+
+        # exit program on 'esc' key pressed
+        elif k == 27:
+            break
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
