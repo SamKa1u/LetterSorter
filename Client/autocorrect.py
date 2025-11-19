641886e4b7e142253d1f753b75e894f5c26c0674
Samuel Kalu <samkalu@ttu.edu>
2025-11-18 23:26:29 -0600

    Update autocorrect.py

--- Client/autocorrect.py
+++ Client/autocorrect.py
@@ -73,37 +73,3 @@ def corrected_state(input_name):
             closest_match = correct_name
             
     return closest_match
-    
-
-    "Tennessee",
-    "Texas",
-    "Utah",
-    "Virginia",
-    "Vermont",
-    "Washington",
-    "Wisconsin",
-    "West Virginia",
-    "Wyoming"
-}
-
-def corrected_state(input_name):
-    """
-    Autocorrects inputed state name by finding closest match in list valid_names.
-    
-    Parameters:
-    input_name (str): Potentionally mispelled state
-
-    Returns:
-    str: Closest matching valid state name.
-    """
-    highest_ratio = 0.0
-    closest_match = input_name
-    
-    for correct_name in valid_names:
-        ratio = SequenceMatcher(None, input_name.lower(), correct_name.lower()).ratio() # get ratio of similarity between input and states in valid_names 
-        if ratio > highest_ratio:                                                       # update/store state with highest similarity 
-            highest_ratio = ratio
-            closest_match = correct_name
-            
-    return closest_match
-    
