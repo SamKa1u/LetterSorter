bf2d18beff99b086e3d7953b37a95432e4272b1f
Samuel Kalu <samkalu@ttu.edu>
2025-11-20 01:52:18 +0000

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -1,207 +1,97 @@
-# autocorrection
-from autocorrect import corrected_state
+import requests as rq
+import cv2
+import threading
 
-def get_region(state):
-    """
-    Returns the region associated with a given state name.
+# localization
+from localization import localize_response
 
-    Parameters:
-        state_name (str): The name of the state
+# uart coms
+from UART import Coms
 
-    Returns:
-        str: The region of the state ('North', 'South', 'Midwest', or 'West'),
-             or None if state not found
-    """
-    regions = {
-        "Alaska": "West",
-        "Alabama": "South",
-        "Arkansas": "South",
-        "Arizona": "West",
-        "California": "West",
-        "Colorado": "West",
-        "Connecticut": "North",
-        "District of Columbia": "South",
-        "Delaware": "South",
-        "Florida": "South",
-        "Georgia": "South",
-        "Hawaii": "West",
-        "Iowa": "Midwest",
-        "Idaho": "West",
-        "Illinois": "Midwest",
-        "Indiana": "Midwest",
-        "Kansas": "Midwest",
-        "Kentucky": "South",
-        "Louisiana": "South",
-        "Massachusetts": "North",
-        "Maryland": 'South',
-        "Maine": "North",
-        "Michigan": "Midwest",
-        "Minnesota": "Midwest",
-        "Missouri": "Midwest",
-        "Mississippi": "South",
-        "Montana": "West",
-        "North Carolina": "South",
-        "North Dakota": "Midwest",
-        "Nebraska": "Midwest",
-        "New Hampshire": "North",
-        "New Jersey": "North",
-        "New Mexico": "West",
-        "Nevada": "West",
-        "New York": "North",
-        "Ohio": "Midwest",
-        "Oklahoma": "South",
-        "Oregon": "West",
-        "Pennsylvania": "North",
-        "Rhode Island": "North",
-        "South Carolina": "South",
-        "South Dakota": "Midwest",
-        "Tennessee": "South",
-        "Texas": "South",
-        "Utah": "West",
-        "Virginia": "South",
-        "Vermont": "North",
-        "Washington": "West",
-        "Wisconsin": "Midwest",
-        "West Virginia": "South",
-        "Wyoming": "West"
+# fastapi URL config
+base_url = "https://samka1u--ocr-fastapi-app.modal.run"
+endpoints = ["/","/receive_img"]
+URLparams =  [
+    {"OCR_backend": "paddle"},
+    {"OCR_backend": "easy"}
+]
+
+# shared state
+shared_state = {
+        "RX": None,
+        "TX": None 
     }
-    return regions.get(state)
 
-def encode_region(region):
-    """
-    Returns the value associated with a given region for UART comms.
+# ------------ start uart --------- #
+uart = Coms(shared_state)
+uart_thread = threading.Thread(target = uart.run, daemon = True)
+uart_thread.start()
 
-    Parameters:
-        region (str): The name of the region
+# load OCR frameworks
+rq.get(base_url + endpoints[0])
 
-    Returns:
-        str: The comm value of the region ('0', '1', '2', or '3'),
-             or None if state not found
-    """
-    encoded_regions = {
-        "South": "0",
-        "North": "1",
-        "West": "2",
-        "Midwest": "3",
-    }
-    return encoded_regions.get(region)
-def detect_abbrev(state):
-    """
-    Detects abbreviated state names and replaces with full name 
+def main():
+    cap = cv2.VideoCapture(0)
+#     cap.set(3, 1280)
+#     cap.set(4, 1024)
+    if not cap.isOpened():
+        print("Error: Could not open video stream.")
+        exit()
+    while True:
+        # preview with openCV
+        ret, frame =  cap.read()
+        if not ret:
+            print('[main] frame skipped')
+            continue
+        cv2.imshow("frame", frame)
 
-    Parameters:
-        state (str): The name of the potentially abbreviated state 
+        # capture and send image of letter if c key is pressed
+        k = cv2.waitKey(1)
+#         if k  == ord('c'):
+        if b'IR_DETECTED' in shared_state.get("RX"):              # capture and send image of letter if IR detects letter is pressed
+            # save letter image
+            cv2.imwrite("letter_image.png", frame)
+            print("[main] capture taken")
 
-    Returns:
-        full_state (str): full name of state
-    """
-    state_abbrev = {
-        "AK": "Alaska",
-        "AL":"Alabama",
-        "AR":"Arkansas",
-        "AZ":"Arizona",
-        "CA":"California",
-        "CO":"Colorado",
-        "CT":"Connecticut",
-        "DC":"District of Columbia",
-        "DE":"Delaware",
-        "FL":"Florida",
-        "GA":"Georgia",
-        "HI":"Hawaii",
-        "IA":"Iowa",
-        "ID":"Idaho",
-        "IL":"Illinois",
-        "IN":"Indiana",
-        "KS":"Kansas",
-        "KY":"Kentucky",
-        "LA":"Louisiana",
-        "MA":"Massachusetts",
-        "MD":"Maryland",
-        "ME":"Maine",
-        "MI":"Michigan",
-        "MN":"Minnesota",
-        "MO":"Missouri",
-        "MS":"Mississippi",
-        "MT":"Montana",
-        "NC":"North Carolina",
-        "ND":"North Dakota",
-        "NE":"Nebraska",
-        "NH":"New Hampshire",
-        "NJ":"New Jersey",
-        "NM":"New Mexico",
-        "NV":"Nevada",
-        "NY":"New York",
-        "OH":"Ohio",
-        "OK":"Oklahoma",
-        "OR":"Oregon",
-        "PA":"Pennsylvania",
-        "RI":"Rhode Island",
-        "SC":"South Carolina",
-        "SD":"South Dakota",
-        "TN":"Tennessee",
-        "TX":"Texas",
-        "UT":"Utah",
-        "VA":"Virginia",
-        "VT":"Vermont",
-        "WA":"Washington",
-        "WI":"Wisconsin",
-        "WV":"West Virginia",
-        "WY":"Wyoming"    
-    }
-    # determine if state name is an abbreviation
-    name_length = len(state)
-    print("name length:",name_length)
-    if name_length < 3:
-        state_name = state_abbrev.get(state) # get full state name
-        print("abbreviation detected")
-    else:
-        state_name = state
-        
-    return state_name
+            # send letter image to LetterSorter app
+            try:
+                files = {'file': open('letter_image.png', 'rb')}
+            except Exception as e:
+                print(f"letter image file could not be opened: {e}")  # exit program if letter image not saved
+                break
+            
+            try:
+                location = rq.post(base_url + endpoints[1], files=files, params=URLparams[0])
+            except ConnectionError as e:
+                print(f"[server connection error]: {e}")
+                try:
+                    status = rq.get(base_url + endpoints[0]).status_code
+                    print("connection status:",status)
+                except Exception as e:
+                    print(f"[server connection error]: {e}")
+            except Exception as e:
+                print(f"[server connection error]: {e}")
+                
+            status = location.status_code
+            if status != 200:
+                print(f"[main] status code: {status},response: {location.content}")
+                shared_state["TX"] = "REGION:4"
+                continue
+            else:
+                json_resp = location.json()
+                region = localize_response(json_resp) # calls localization where state determined by backend is autocorrected
+                if region is None:
+                    shared_state["TX"] = "REGION:4"
+                else:       
+                    cmd = "REGION:" + region
+                    shared_state["TX"] = cmd
+                        
+        # exit program on 'esc' key pressed
+        elif k == 27:
+            break
+    cv2.destroyAllWindows()
 
-def localize_response(resp):
-    """
-    Calls helper functions encode_region and get_region to deduce region from raw state string.
-    State is validated by autocorrect in process.
-
-    Parameters:
-        byte_string (str): The byte string response from the backend
-
-    Returns:
-        encoded_reg (str): The comm value of the region ('0', '1', '2', or '3'),
-             or None if state not found
-        
-    """
-    # extract unvalidated state text from backend response
-    flag = resp['Just State Flag']
-    message = resp['message']
-    print("Just State:", flag)
-    
-    if message:
-        print("message:", message)
-        # extract state based on its location in message response
-        if flag:
-            _ = message.split()
-            print("split list:",_)
-            raw_state = _[0] + " " + _[1]
-        else:     
-            _ = message.split()
-            print("split list:",_)
-            raw_state = _[1]
-        
-        # detect abbreviated state replace with full name
-        print("raw state", raw_state)
-        state = detect_abbrev(raw_state)
-        
-        # autocorrect state text 
-        valid_state = corrected_state(state)        
-        print("autocorrected state:",valid_state)
-        
-        # determine region and encode
-        region = get_region(valid_state)
-        print("region:",region)
-        encoded_reg = encode_region(region)
-        print("encoded region:",encoded_reg)
-        return encoded_reg
-    else:
-        return None
+if __name__ == '__main__':
+    main()
+    # location = b"lubbock texas 79401"
+    # encoded_region = localize_response(location)
